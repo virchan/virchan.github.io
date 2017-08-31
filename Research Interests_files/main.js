bb57f8b69f0dac229ec35bc14922d2901edab75c
virchan <vchanwc@gmail.com>
2017-08-31 10:19:26 -0400

    Delete main.js

--- Research Interests_files/main.js
+++
@@ -1,9 +0,0 @@
-function Toggle(abstractID) {
-     var CurrentAbstract = document.getElementById(abstractID);
-     if (CurrentAbstract.style.display == 'none') {
-         CurrentAbstract.style.display = 'block';
-     }
-     else {
-         CurrentAbstract.style.display = 'none';
-     }
- }
